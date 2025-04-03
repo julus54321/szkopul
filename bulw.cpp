02f386089a94d088b972d813d8bb03bbf8d2bcbc
julus <julus.contact@gmail.com>
2025-04-03 13:51:38 +0000

    bulwy 42%

--- bulw.cpp
+++ bulw.cpp
@@ -3,8 +3,32 @@ using namespace std;
 
 int main(){
 
-    int a;
-    cin>>a;
+    int pol,lbulw;
+    cin>>pol;
 
+    vector<int> vec(pol);
+
+    for (int x = 0; x < pol;x++)
+    {
+        cin >> vec[x];
+    }
+
+    cin>>lbulw;
+
+    sort(vec.rbegin(), vec.rend());
+
+    int y = 0;
+
+    for (int i = 0;i < pol && lbulw > 0;i++)
+    {   
+        int bdz = min(vec[i] , lbulw);
+            
+        y += bdz * bdz;
+        lbulw -= bdz;
+        
+    }
+
+    cout << y <<endl;
+    
     return 0;
 }

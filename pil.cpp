85952b2aa688f7290654a12304dec0109357103d
julus <julus.contact@gmail.com>
2025-04-04 04:50:59 +0000

    pileczka 66%

--- pil.cpp
+++ pil.cpp
@@ -3,27 +3,22 @@ using namespace std;
 
 int main(){
 
-    int a,y;
+    int a;
     cin>>a;
-
-    vector<int> vec;
 
     for (int i = 0;i<a;i++)
     {
         int b,c,x = 0;
         cin>>b>>c;
-        for(int s = 0; ;s++)
+        int odb = 0;
+
+        if (b == 0 || c == 0 || b >= c) {
+            cout<<0<<endl;
+        }else 
         {
-            x += b*2;
-            if(x >= c){vec.push_back(x);break;}
+            odb = ceil(log2((double)c/b));
+            cout<<odb<<endl;
         }
-        
     }
-
-    for(y=0;y<vec.size();y++)
-    {
-        cout<<vec[y]<<endl;
-    }
-
     return 0;
 }

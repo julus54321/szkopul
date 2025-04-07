165f72568c57b40f7018567f6339aecd8175eb8b
julus <julus.contact@gmail.com>
2025-04-07 12:08:31 +0000

    bankiet 100%

--- ban.cpp
+++ ban.cpp
@@ -3,5 +3,32 @@ using namespace std;
 
 int main(){
 
+    int a;
+    cin >> a;
+
+    vector<int> kpl(a + 1);
+    vector<bool> co (a + 1, false);
+    int ls = 0;
+
+    for (int i = 1; i <= a; i++) {
+        cin >> kpl[i];
+    }
+
+    for (int i = 1; i <= a; i++)
+    {
+        
+        if (co[i] == false)
+        {   
+            int crg = i;
+            while (!co[crg])
+            {
+                co[crg] = true;
+                crg = kpl[crg]; 
+            }
+            ls++;
+        }
+    }
+    cout<<ls<<endl;
+
     return 0;
 }

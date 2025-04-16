904110bd6109d34a48ebd54a19b5009592e1dbb9
julus <julus.contact@gmail.com>
2025-04-16 12:48:25 +0000

    idk

---
+++ rol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+
+    int a,b,x;
+    cin>>a>>b;
+    vector<int> vec;
+
+    for (int i = 1;i<a;i++)
+    {
+        cin>>x;
+        vec.push_back(x);
+    }
+
+    return 0;
+}
